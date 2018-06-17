64d9acb4e0716cf24943e933b8a435baf3b36544
DESKTOP-UDJAS9N\HieuPC <hieuht300894@gmail.com>
2018-06-17 21:21:09 +0700

    no message

---
+++ CefSharpDemo/CefSharpDemo/Script/jsMouseEvent.js
@@ -0,0 +1,5 @@
+﻿function triggerMouseEvent(node, eventType) {
+    var clickEvent = document.createEvent('MouseEvents');
+    clickEvent.initEvent(eventType, true, true);
+    node.dispatchEvent(clickEvent);
+}
