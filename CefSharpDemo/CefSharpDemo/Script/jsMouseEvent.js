097a6211b2a1b90529945512c0d2fa34caeb295c
DESKTOP-UDJAS9N\HieuPC <hieuht300894@gmail.com>
2018-06-18 22:28:30 +0700

    no message

--- CefSharpDemo/CefSharpDemo/Script/jsMouseEvent.js
+++ CefSharpDemo/CefSharpDemo/Script/jsMouseEvent.js
@@ -1,55 +1,64 @@
-﻿function triggerMouseEvent(node, eventType, x, y) {
-    if (x == undefined) {
-        x = node.offsetLeft;
-    }
-    if (y == undefined) {
-        y = node.offsetTop;
-    }
-    var clickEvent = document.createEvent('MouseEvents');
-    clickEvent.initEvent(
-        eventType,
-        true /* bubble */,
-        true /* cancelable */,
-        window, null,
-        x, y, 0, 0, /* coordinates */
-        false, false, false, false, /* modifier keys */
-        0 /*left*/,
-        null);
-    node.dispatchEvent(clickEvent);
-}
+﻿< !DOCTYPE html >
+    <html>
+        <head>
+            <meta charset=utf - 8 />
+                <title>JS Bin</title>
+                <script src="jquery.min.js"></script>
+</head>
+<body>
+                <div id="area" style="width: 1000px; height: 1000px; background-color:blue; position: relative;">
+                    <input type="button" id="btnClick" value="Click me !!!" style="width: 100px; height: 50px" />
+                    <input type="text" id="txtInput" placeholder="Input something" style="width: 200px; height: 44px" />
+                    <input type="button" id="btnClick2" value="!!!" style="width: 50px; height: 50px; left: 100px; top: 100px; position: absolute;" />
+                </div>
 
-//function triggerMouseEvent(x, y) {
-//    var ev = document.createEvent("MouseEvent");
-//    var el = document.elementFromPoint(x, y);
-//    ev.initMouseEvent(
-//        "click",
-//        true /* bubble */, true /* cancelable */,
-//        window, null,
-//        x, y, 0, 0, /* coordinates */
-//        false, false, false, false, /* modifier keys */
-//        0 /*left*/, null
-//    );
-//    el.dispatchEvent(ev);
-//}
+                <script>
+                    function ShowMessage(){
+                        console.log($('#txtInput').val());
+                    }
 
-function triggerMouseEvent(node, eventType, x, y) {
-    var clickEvent = document.createEvent('MouseEvents');
-    if (x == undefined || y == undefined) {
-        clickEvent.initEvent(
-            eventType,
-            true /* bubble */,
-            true /* cancelable */);
-    }
-    else {
-        clickEvent.initEvent(
-            eventType,
-            true /* bubble */,
-            true /* cancelable */,
-            window, null,
-            x, y, 0, 0, /* coordinates */
-            false, false, false, false, /* modifier keys */
-            0 /*left*/,
-            null);
-    }
-    node.dispatchEvent(clickEvent);
-}
+	$('#btnClick').off('click').on('click', function(e){
+                        console.clear();
+
+                    var elemArea = document.getElementById('area');
+		MouseClick(document, 'mousedown', 108, 108);
+	});
+
+	$('#btnClick2').off('click').on('click', function(e){
+                        console.log('click');
+                    });
+
+	$('#btnClick2').off('mousedown').on('mousedown', function(e){
+                        console.log('mousedown');
+                    });
+
+	$('#btnClick2').off('mouseup').on('mouseup', function(e){
+                        console.log('mouseup');
+                    });
+
+	function MouseClick(element, eventType, clientX, clientY) {
+		var event = document.createEvent('MouseEvents');
+		var _element = document.elementFromPoint(clientX, clientY);
+		event.initMouseEvent(
+		/*type*/ eventType,
+		/*canBubble*/ true,
+		/*cancelable*/ true,
+		/*view*/ window,
+		/*detail*/ 0,
+		/*screenX*/ 0,
+		/*screenY*/ 0,
+		/*clientX*/ clientX,
+		/*clientY*/ clientY,
+		/*ctrlKey*/ false,
+		/*altKey*/ false,
+		/*shiftKey*/ false,
+		/*metaKey*/ false,
+		/*button*/ 3,
+		/*relatedTarget*/ null);
+
+		_element.dispatchEvent(event);
+		console.log(_element);
+	}
+  </script>
+            </body>
+</html>
